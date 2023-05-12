3c26d3821e08a8045dc2ddbc4e049459bc475f94
Alejandro-RECO <revolledocoronado@hotmail.com>
2023-05-12 16:00:16 -0500

    UPDATE ENUMERATE CHALENGER
    Actualizando el orden de los retos

--- Retos/src/Retos/RetoIV.java
+++ Retos/src/Retos/RetoIV.java
@@ -1,26 +1,47 @@
 package Retos;
 import java.util.Scanner;
 
-
 public class RetoIV {
     public static void main(String[] args) {
-        Scanner leer = new Scanner(System.in);
-        int numero = (int)(Math.random()*2+1);
-        int opcion;
+        int piedra = 1, papel =2, tijera = 3;
+        int respuesta;
+        int ranMaq = (int)(Math.random()*3+1);
+       Scanner sc = new Scanner(System.in);
 
-        System.out.println("Elige una opcion (1.Cara 2.Sello)");
-        opcion = leer.nextInt();
-            if(opcion == numero){
-                System.out.println("Ganaste ");
-            }else{
-                System.out.println("Perdiste");
+
+        System.out.println("Escoja una opción \n 0 = piedra \n 1 = Tijera \n 2 = Papel");
+        respuesta = sc.nextInt();
+
+            if (respuesta == 0 ){
+            System.out.println("Escogiste piedra");
+                if (ranMaq == piedra){
+                    System.out.println(" Empate\nEl sistema escoje piedra");
+                }else if (ranMaq == papel){
+                    System.out.println(" Pierdes\nEl sistema escoje papel");
+                }else if (ranMaq == tijera){
+                    System.out.println(" Ganaste\nEl sistema escoje tijera");
+                }
             }
-            if(numero == 1){
-                System.out.println("El resultado es " + numero + ".Cara");
-            }else if(numero == 2){
-                System.out.println("El resultado es " + numero + ".Sello");
+            if (respuesta == 1 ){
+            System.out.println("Escogiste tijera");
+                if (ranMaq == piedra){
+                    System.out.println(" Pierdes\nEl sistema escoje piedra");
+                }else if (ranMaq == papel){
+                    System.out.println(" Ganas\nEl sistema escoje papel");
+                }else if (ranMaq == tijera){
+                    System.out.println(" Empate\nEl sistema escoje tijera");
+                }
             }
-        leer.close();
+            if (respuesta == 2 ){
+            System.out.println("Escogiste papel");
+                if (ranMaq == piedra){
+                    System.out.println(" Ganas\nEl sistema escoje piedra");
+                }else if (ranMaq == papel){
+                    System.out.println(" Empate\nEl sistema escoje papel");
+                }else if (ranMaq == tijera){
+                    System.out.println(" Pierdes\nEl sistema escoje tijera");
+                }
+            }
+        sc.close();
     }
-    
 }
