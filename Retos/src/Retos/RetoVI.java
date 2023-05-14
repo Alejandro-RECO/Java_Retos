75958c9cfec285f32ba3d75c60d10f7eb9938d73
Alejandro-RECO <revolledocoronado@hotmail.com>
2023-05-13 20:05:11 -0500

    Update RetoVI
    Creando código de conteo de precios

--- Retos/src/Retos/RetoVI.java
+++ Retos/src/Retos/RetoVI.java
@@ -1,5 +1,20 @@
 package Retos;
+import java.util.Scanner;
 
 public class RetoVI {
-    
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double total = 0;
+
+        System.out.println("Ingrese la cantidad de productos:");
+        int cantidadProductos = scanner.nextInt();
+
+        for (int i = 1; i <= cantidadProductos; i++) {
+            System.out.println("Ingrese el precio del producto " + i + ":");
+            double precio = scanner.nextDouble();
+            total += precio;
+        }
+
+        System.out.println("El total es: " + total);
+    }
 }
